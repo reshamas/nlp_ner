809c2ff915127fa8b00ad83e67e1aadbf61399dc
reshama <rs2715@gmail.com>
2016-08-04 12:26:54 -0400

    updating script - starting subsetting

--- a_readin2.py
+++ a_readin2.py
@@ -12,8 +12,9 @@
 # want to add:  tweet_char
 
 #with open("data/all.iob", "r") as infile:
-with open("data/lines60.iob", "r") as infile:
 #with open("data/lines350.iob", "r") as infile:
+#with open("data/lines60.iob", "r") as infile:
+with open("data/lines50.iob", "r") as infile:
     ner_list = []
     tweet_num = 1
     tweet_item = 0
@@ -31,30 +32,34 @@ with open("data/lines60.iob", "r") as infile:
 
             
             #print token, tclass1
-
-            item = [tweet_num, tweet_item, token, tclass1]
-            
-            # add a class for twitter mentions
-            if token[0] == "@":
-                tclass2 = "O-mention"
-                item.append(tclass2) 
-                #print '------'
-                #print item
-                #print tclass2
+            for char in token:
+                #print tweet_num, tweet_item, token, char, tclass1
                 
-            # Add a class for emojis
-            if len(token) > 1 and token[0] == ":":
-                tclass3 = "O-emoji"
-                item.append(tclass3)
-                #print item, tclass1, tclass3
+                item = [tweet_num, tweet_item, token, char, tclass1]
             
-            # Add a class for url links
-            if token.startswith("http"):
-                tclass4 = "O-url"
-                item.append(tclass4)
-                #print item, tclass1, tclass4
+                # add a class for twitter mentions
+                if token[0] == "@":
+                    tclass2 = "O-mention"
+                    item.append(tclass2) 
+                    #print '------'
+                    #print item
+                    #print tclass2
+                
+                # Add a class for emojis
+                if len(token) > 1 and token[0] == ":":
+                    tclass3 = "O-emoji"
+                    item.append(tclass3)
+                    #print item, tclass1, tclass3
+            
+                # Add a class for url links
+                if token.startswith("http"):
+                    tclass4 = "O-url"
+                    item.append(tclass4)
+                    #print item, tclass1, tclass4
 
-            ner_list.append(item)
+                ner_list.append(item)
+
+
 
 print "------------------------------"
 
@@ -69,7 +74,12 @@ for item in ner_list[:50]:
 print "------------------------------"
 
 
+
+
 def ngram(numg, logp):
+    for i in range (2,3):
+        for tweetnum in range(1,3):
+            
     return
     
 
