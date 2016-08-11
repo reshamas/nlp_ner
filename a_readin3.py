272e039e0b023adc0e569fc9fe842252cb1f1ede
reshama <rs2715@gmail.com>
2016-08-11 19:48:38 -0400

    add in ngram code, used two dictionaries

--- a_readin3.py
+++ a_readin3.py
@@ -39,24 +39,27 @@ with open("data/lines50.iob", "r") as infile:
             
                 # add a class for twitter mentions
                 if token[0] == "@":
-                    tclass2 = "O-mention"
-                    item.append(tclass2) 
+                    tclass2 = "B-mention"
+                    #item.append(tclass2)
                     #print '------'
                     #print item
                     #print tclass2
                 
                 # Add a class for emojis
-                if len(token) > 1 and token[0] == ":":
-                    tclass3 = "O-emoji"
-                    item.append(tclass3)
+                elif len(token) > 1 and token[0] == ":":
+                    tclass2 = "B-emoji"
+                    #item.append(tclass3)
                     #print item, tclass1, tclass3
             
                 # Add a class for url links
-                if token.startswith("http"):
-                    tclass4 = "O-url"
-                    item.append(tclass4)
+                elif token.startswith("http"):
+                    tclass2 = "B-url"
+                    #item.append(tclass4)
                     #print item, tclass1, tclass4
 
+                else:
+                    tclass2 = tclass1
+                item.append(tclass2)
                 ner_list.append(item)
 
 
@@ -65,33 +68,72 @@ print "------------------------------"
 
 print "len(ner_list):  ",  len(ner_list)
 
+from collections import defaultdict
+
+from multi_key_dict import multi_key_dict
+
+
+# Empty dict
+#d = {}
+#d = defaultdict(list)
+d = multi_key_dict()
+
+# Fill in the entries one by one
+#d["age"] = 25
+for line in ner_list:
+    setkey = tuple((line[0], line[1], line[2], line[4], line[5]))
+    #print setkey
+    d[setkey].append(line[3])
+    
+
+print "len(d): ", len(d)
+
+#for i in d:
+#        print i, d[i]
+        
+#for k, v in d.iteritems():
+#        print k, v
+
+#print sorted(d, key=lambda key: d[key])
+
+from pprint import pprint
+
+pprint (sorted(d.items()))
+
 
 print "------------------------------"
-for item in ner_list[:50]:
-    print item
+i = 1
 
+#for item in ner_list[:50]:
+    #tweetnum = item
+    #print item
+    #print item[1]
+    #print item
 
 print "------------------------------"
+
+
+#for tweet in tweetlist:
+
+
+
+
+
+#maxgrams = len(tweet)
 
 
 def find_ngrams(input_list, n):
       return zip(*[input_list[i:] for i in range(n)])
   
 
-test = find_ngrams(ner_list, 3)
-print test
+#test = find_ngrams(ner_list, 3)
+#print test
     
 def ngram(numg, logp):
     #for i in range (2,3):
     #    for tweetnum in range(1,3):
             
     return
-
-
-
-
-
-
 
 
 #ngram(4, 1)
---
+++ a_readin5.py
@@ -0,0 +1,141 @@
+#------------------------------------------------------------------------------
+# Description:   
+# Project:       NLP - NER
+# Date Updated:  07/28/2016
+# Author:
+# Updated By:    Reshama
+#
+# Input:         data/all.iob
+# Output:        
+# -----------------------------------------------------------------------------
+# ner_list:  tweet_num, tweet_item, token, class
+# want to add:  tweet_char
+# -----------------------------------------------------------------------------
+from nltk.util import ngrams
+from collections import defaultdict
+
+#with open("data/all.iob", "r") as infile:
+#with open("data/lines350.iob", "r") as infile:
+#with open("data/lines60.iob", "r") as infile:
+#with open("data/lines50.iob", "r") as infile:
+with open("data/lines25.iob", "r") as infile:
+    ner_list = []
+    tweet_num = 1
+    tweet_item = 0
+    for line in infile:
+        line=line.rstrip()
+        lineitems = line.split("\t")
+        #print lineitems
+        if line == '':
+            tweet_num += 1
+            tweet_item = 0
+        else:
+            tweet_item += 1
+            token = lineitems[0]
+            tclass1 = lineitems[1]
+
+            
+            #print token, tclass1
+            for char in token:
+                #print tweet_num, tweet_item, token, char, tclass1
+                
+                item = [tweet_num, tweet_item, token, char, tclass1]
+            
+                # add a class for twitter mentions
+                if token[0] == "@":
+                    tclass2 = "B-mention"
+                    #item.append(tclass2)
+                    #print '------'
+                    #print item
+                    #print tclass2
+                
+                # Add a class for emojis
+                elif len(token) > 1 and token[0] == ":":
+                    tclass2 = "B-emoji"
+                    #item.append(tclass3)
+                    #print item, tclass1, tclass3
+            
+                # Add a class for url links
+                elif token.startswith("http"):
+                    tclass2 = "B-url"
+                    #item.append(tclass4)
+                    #print item, tclass1, tclass4
+
+                else:
+                    tclass2 = tclass1
+                item.append(tclass2)
+                ner_list.append(item)
+
+
+
+print "------------------------------"
+
+print "len(ner_list):  ",  len(ner_list)
+
+# Empty dict
+d = defaultdict(list)
+dtweet = defaultdict(list)
+dtweetcl = defaultdict(list)
+dclass = defaultdict(list)
+
+# Fill in the entries one by one
+
+for line in ner_list:
+    #setkey = tuple((line[0], line[1], line[2], line[4], line[5]))
+    #setkey = tuple(str(line[0]))
+    setkey = tuple((line[0], line[1]))
+    token = line[3]
+    tclass1 = tuple((line[4], line[5]))
+    #token_class = tuple((token, line[4], line[5]))
+    token_class = list((token, line[4], line[5]))
+    #print setkey, token_class
+    d[setkey].append(line[3])
+    dclass[setkey].append(tclass1)
+
+    dtweet[str(line[0])].append(line[3])
+    dtweetcl[str(line[0])].append(line[4])
+    
+    
+#print "\nlen(d=dictionary): ", len(d)
+#print "\nlen(dclass dict):  ", len(dclass)
+
+print "\nlen(dtweet dict):  ", len(dtweet)
+print "\nlen(dclass dict):  ", len(dtweetcl)
+
+
+def printDict(dictname):
+    for k, v in dictname.items():
+        print k, v
+
+
+print "------------------------------"
+printDict(dtweet)
+print "------------------------------"
+printDict(dtweetcl)
+
+print "------------------------------"
+# tkey = tweet number 
+# tvalue = each character of tweet
+# ckey = tweet number
+# cvalue = class
+
+from itertools import izip
+for (tkey, tvalue), (ckey, cvalue) in izip(dtweet.iteritems(), dtweetcl.iteritems()):
+       tweet_length = sum(1 for v in tvalue if v)
+       print "\ntweet_length: ", tweet_length
+       print tkey
+       print tvalue
+       print ckey
+       print cvalue
+       for gramlength in range(3, tweet_length):
+           gramitems = ngrams(tvalue, gramlength)
+
+           for index, grams in enumerate(gramitems, start=0):
+               #print "\ngrams: ", grams
+               #print "\tclass: ", cvalue[index]
+
+               gramsjoined = "".join(grams)
+               #print "grams: ", gramsjoined
+               
+       print "\nmaximum-length gram: "
+       print gramsjoined
